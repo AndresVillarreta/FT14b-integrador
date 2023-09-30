0e761c968ce123f3881c79d711b270a7edcfa388
Andres Villarreta <andresvillarreta1@proton.me>
2023-09-30 10:52:05 -0400

    Finally 2

--- Server/src/index.js
+++ Server/src/index.js
@@ -2,10 +2,10 @@ const server = require("./app");
 const PORT = 3001;
 const { conn } = require("./DB_connection");
 
-conn.sync({ alter: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   console.log("Database connected");
 
-  server.listen(PORT, () => {
+  server.listen(process.env.PORT || PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 });
